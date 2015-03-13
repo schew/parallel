8d5f86a1fde77de0e857a183f264824371f94050
schew4 <schew4@jhu.edu>
2015-03-13 00:45:08 -0400

    e

--- run.sh
+++ run.sh
@@ -1,15 +1,6 @@
-java CoinFlip 1 1000000000 >> Results2.txt
-java CoinFlip 1 1000000000 >> Results2.txt
-java CoinFlip 1 1000000000 >> Results2.txt
-java CoinFlip 2 2000000000 >> Results2.txt
-java CoinFlip 2 2000000000 >> Results2.txt
-java CoinFlip 2 2000000000 >> Results2.txt
-java CoinFlip 4 4000000000 >> Results2.txt
-java CoinFlip 4 4000000000 >> Results2.txt
-java CoinFlip 4 4000000000 >> Results2.txt
-java CoinFlip 8 8000000000 >> Results2.txt
-java CoinFlip 8 8000000000 >> Results2.txt
-java CoinFlip 8 8000000000 >> Results2.txt
-java CoinFlip 16 16000000000 >> Results2.txt
-java CoinFlip 16 16000000000 >> Results2.txt
-java CoinFlip 16 16000000000 >> Results2.txt
+    for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+    do
+        java CoinFlip $i 1000000000 >> Results.txt
+        java CoinFlip $i 1000000000 >> Results.txt
+        java CoinFlip $i 1000000000 >> Results.txt
+    done
