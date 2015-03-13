d3d229cbfe095198d0d10e3b544c7557e801fc60
schew4 <schew4@jhu.edu>
2015-03-12 23:01:52 -0400

    wef

--- CoinFlip.java
+++ CoinFlip.java
@@ -1,25 +1,23 @@
-//For this experiment you will need to use some java functions. Specifically, System.currentTimeMillis() and Random.generate() and Random.nextInt(). The SealedDES.java program I give you in part 2 has examples. You will also need join() all your child threads from the parent thread. This is the most basic form of synchronization.
+//For this experiment you will need to use some java functions. Specifically, System.currentTimeMillis() and Random.generate() and Random.nextlong(). The SealedDES.java program I give you in part 2 has examples. You will also need join() all your child threads from the parent thread. This is the most basic form of synchronization.
 import java.util.concurrent.ThreadLocalRandom;
 
 public class CoinFlip implements Runnable{
-    static int numHeads;
-    static int numTails;
-    static int numThreads;
-    static int numFlips;
-
-    //public ThreadLocalRandom gen = new ThreadLocalRandom();
+    static long numHeads;
+    static long numTails;
+    static long numThreads;
+    static long numFlips;
 
     // Constructor for Flip
-    public CoinFlip(int nT, int nF) {
+    public CoinFlip(long nT, long nF) {
         this.numThreads = nT;
         this.numFlips = nF;
     }
 
     public void run() {
-        int headCount = 0;
-        int tailCount = 0;
-        for (int i = 0; i < numFlips/numThreads; i++) {
-            if (ThreadLocalRandom.current().nextInt(2) == 1) {
+        long headCount = 0;
+        long tailCount = 0;
+        for (long i = 0; i < numFlips/numThreads; i++) {
+            if (ThreadLocalRandom.current().nextlong(2) == 1) {
                 ++headCount;
             } else {
                 ++tailCount;
@@ -34,35 +32,35 @@ public class CoinFlip implements Runnable{
     public static void main (String[] args) {
         // Error if user doesn't put in the right arguments
         if (args.length != 2) {
-            System.out.println("Usage: CoinFlip #threads #iterations");
+            System.out.prlongln("Usage: CoinFlip #threads #iterations");
             return;
         }
-        int nT = Integer.parseInt(args[0]);
-        int nF = Integer.parseInt(args[1]);
+        long nT = Long.parseLong(args[0]);
+        long nF = Long.parseLong(args[1]);
 
         // Create array of threads and start timer
         Thread[] threads = new Thread[nT];
         long startTime = System.currentTimeMillis();
         
         // Start threads
-        for (int i = 0; i < nT; i++) {
+        for (long i = 0; i < nT; i++) {
             threads[i] = new Thread(new CoinFlip(nT, nF));
             threads[i].start();
         }
 
         // Join threads
-        for (int i = 0; i < nT; i++) {
+        for (long i = 0; i < nT; i++) {
             try {
                 threads[i].join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            } catch (longerruptedException e) {
+                e.prlongStackTrace();
             }
         }
 
-        // Print out information
+        // Prlong out information
         long runTime = System.currentTimeMillis() - startTime;
-        System.out.println(numHeads + " heads in " + nF + " coin tosses");
-        System.out.println("Elapsed time: " + runTime);
-        System.out.println(numHeads + numTails);
+        System.out.prlongln(numHeads + " heads in " + nF + " coin tosses");
+        System.out.prlongln("Elapsed time: " + runTime);
+        System.out.prlongln(numHeads + numTails);
     }
 }
