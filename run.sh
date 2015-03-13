23852d3c4ff60ab79e7855b66384e92abb55b711
schew4 <schew4@jhu.edu>
2015-03-12 23:02:40 -0400

    oeirj

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
     for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
     do
+        java CoinFlip $i 1000000000 >> Results.txt
+        java CoinFlip $i 1000000000 >> Results.txt
+        java CoinFlip $i 1000000000 >> Results.txt
+    done
+
+    for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
+    do
         java CoinFlip $i 1000000000 >> Results2.txt
         java CoinFlip $i 1000000000 >> Results2.txt
         java CoinFlip $i 1000000000 >> Results2.txt
