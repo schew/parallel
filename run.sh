15a33f86d36baed65d60dc8d2b60d733aed15084
schew4 <schew4@jhu.edu>
2015-03-12 22:41:21 -0400

    soife

--- run.sh
+++ run.sh
@@ -1,18 +1,18 @@
     for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16
     do
-        java CoinFlip i 1000000000 >> Results2.txt
-        java CoinFlip i 1000000000 >> Results2.txt
-        java CoinFlip i 1000000000 >> Results2.txt
-        java CoinFlip i 2000000000 >> Results2.txt
-        java CoinFlip i 2000000000 >> Results2.txt
-        java CoinFlip i 2000000000 >> Results2.txt
-        java CoinFlip i 4000000000 >> Results2.txt
-        java CoinFlip i 4000000000 >> Results2.txt
-        java CoinFlip i 4000000000 >> Results2.txt
-        java CoinFlip i 8000000000 >> Results2.txt
-        java CoinFlip i 8000000000 >> Results2.txt
-        java CoinFlip i 8000000000 >> Results2.txt
-        java CoinFlip i 16000000000 >> Results2.txt
-        java CoinFlip i 16000000000 >> Results2.txt
-        java CoinFlip i 16000000000 >> Results2.txt
+        java CoinFlip $i 1000000000 >> Results2.txt
+        java CoinFlip $i 1000000000 >> Results2.txt
+        java CoinFlip $i 1000000000 >> Results2.txt
+        java CoinFlip $i 2000000000 >> Results2.txt
+        java CoinFlip $i 2000000000 >> Results2.txt
+        java CoinFlip $i 2000000000 >> Results2.txt
+        java CoinFlip $i 4000000000 >> Results2.txt
+        java CoinFlip $i 4000000000 >> Results2.txt
+        java CoinFlip $i 4000000000 >> Results2.txt
+        java CoinFlip $i 8000000000 >> Results2.txt
+        java CoinFlip $i 8000000000 >> Results2.txt
+        java CoinFlip $i 8000000000 >> Results2.txt
+        java CoinFlip $i 16000000000 >> Results2.txt
+        java CoinFlip $i 16000000000 >> Results2.txt
+        java CoinFlip $i 16000000000 >> Results2.txt
     done
