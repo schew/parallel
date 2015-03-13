1b1b35d748d5eee9ff4f78397636277782499a16
schew4 <schew4@jhu.edu>
2015-03-12 20:47:22 -0400

    update assignment 2

---
+++ CoinFlip.java
@@ -0,0 +1,68 @@
+//For this experiment you will need to use some java functions. Specifically, System.currentTimeMillis() and Random.generate() and Random.nextInt(). The SealedDES.java program I give you in part 2 has examples. You will also need join() all your child threads from the parent thread. This is the most basic form of synchronization.
+import java.util.concurrent.ThreadLocalRandom;
+
+public class CoinFlip implements Runnable{
+    static int numHeads;
+    static int numTails;
+    static int numThreads;
+    static int numFlips;
+
+    //public ThreadLocalRandom gen = new ThreadLocalRandom();
+
+    // Constructor for Flip
+    public CoinFlip(int nT, int nF) {
+        this.numThreads = nT;
+        this.numFlips = nF;
+    }
+
+    public void run() {
+        int headCount = 0;
+        int tailCount = 0;
+        synchronized (CoinFlip.class) {
+            for (int i = 0; i < numFlips/numThreads; i++) {
+                if (ThreadLocalRandom.current().nextInt(2) == 1) {
+                    ++headCount;
+                } else {
+                    ++tailCount;
+                }
+            }
+        }
+        numHeads += headCount;
+        numTails += tailCount;
+    }
+
+    public static void main (String[] args) {
+        // Error if user doesn't put in the right arguments
+        if (args.length != 2) {
+            System.out.println("Usage: CoinFlip #threads #iterations");
+            return;
+        }
+        int nT = Integer.parseInt(args[0]);
+        int nF = Integer.parseInt(args[1]);
+
+        // Create array of threads and start timer
+        Thread[] threads = new Thread[nT];
+        long startTime = System.currentTimeMillis();
+        
+        // Start threads
+        for (int i = 0; i < nT; i++) {
+            threads[i] = new Thread(new CoinFlip(nT, nF));
+            threads[i].start();
+        }
+
+        // Join threads
+        for (int i = 0; i < nT; i++) {
+            try {
+                threads[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // Print out information
+        long runTime = System.currentTimeMillis() - startTime;
+        System.out.println(numHeads + " heads in " + numFlips + " coin tosses");
+        System.out.println("Elapsed time: " + runTime);
+        System.out.println(numHeads + numTails);
+    }
+}
